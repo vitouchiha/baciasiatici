ee561a57cdd06d95f64de61c2ac26f9a07ec23cb
NekroMonger25 <nekromonger25@gmail.com>
2025-06-20 21:19:44 +0200

    Update stremio.js

--- api/stremio.js
+++ api/stremio.js
@@ -140,7 +140,7 @@ async function getSubtitlesWithPuppeteer(serieId, episodeId) {
 
 const builder = new addonBuilder({
     id: 'com.kisskh.addon',
-    version: '1.2.5',
+    version: '1.2.6',
     name: 'KissKH Addon',
     description: 'Asian content',
     resources: [
